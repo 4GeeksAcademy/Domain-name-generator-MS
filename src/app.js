9d0fca889d7773706339afd574377e189ce0e7e4
monalisasingh87 <monalisa.singh87@gmail.com>
2024-07-30 15:46:56 +0000

    domain name genrator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        let div = document.createElement(`div`);
+        div.innerText = `
+        ${pronoun[i]}${adj[j]}${noun[k]}.com
+        ${pronoun[i]}${adj[j]}${noun[k]}.net
+        ${pronoun[i]}${adj[j]}${noun[k]}.org
+        ${pronoun[i]}${adj[j]}${noun[k]}.edu
+        `;
+        document.body.appendChild(div);
+      }
+    }
+  }
 };
